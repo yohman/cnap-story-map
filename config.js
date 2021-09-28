da92526d892cb41cb81b3466f9cc61cbe224e631
laharps <76974589+laharps@users.noreply.github.com>
2021-09-27 19:43:43 -0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ var config = {
     use3dTerrain: false,
     title: 'Making Nuisance Neighborhoods',
     subtitle: 'Exposing the Effects of Community Policing in Los Angeles',
-    byline: 'Anti-Eviction Mapping Project and Institute on Inequality and Democracy',
+    byline: 'Anti-Eviction Mapping Project and UCLA Luskin Institute on Inequality and Democracy',
     footer: '<img src = "images/IID_logo.png" width = "450" height = "150"> <br> <img src = "images/aemp_logo.png" width = "350" height = "100"> <br> <br> Created with Mapbox Community Solutions',
     chapters: [
         {
